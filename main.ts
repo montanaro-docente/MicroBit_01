c4fa10405cafbe800905c2b8439a22adafef704e
montanaro-docente <81511015+montanaro-docente@users.noreply.github.com>
2021-03-28 12:25:49 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Tasto A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("tasto A+B")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Tasto B")
+})
+basic.showString("Hello!")
